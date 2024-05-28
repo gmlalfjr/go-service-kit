339c9698b907c16b8e81009b159d69de97a2ab0b
Gams <gafajri@pvg.co.id>
2024-05-28 22:48:29 +0700

    add postgres

--- fiber/fiber_service.go
+++ fiber/fiber_service.go
@@ -9,13 +9,13 @@ import (
 
 // FiberService is a service for managing Fiber app
 type FiberService struct {
-	app *fiber.App
+	App *fiber.App
 }
 
 // NewFiberService creates a new FiberService instance
 func NewFiberService() *FiberService {
 	return &FiberService{
-		app: fiber.New(),
+		App: fiber.New(),
 	}
 }
 
@@ -24,11 +24,11 @@ func (s *FiberService) Start() error {
 	port := env.GetString("PORT", "8080")
 	port = fmt.Sprintf(":%s", port)
 	log.Printf("[Fiber] Starting Fiber service on port %s...", port)
-	return s.app.Listen(port)
+	return s.App.Listen(port)
 }
 
 // Stop stops the Fiber service
 func (s *FiberService) Stop() error {
 	log.Println("[Fiber] Stopping Fiber service...")
-	return s.app.Shutdown()
+	return s.App.Shutdown()
 }
